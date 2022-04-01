a97ac375a8ee4b7d3770625881d1e8a3c4b9412d
carloronconi <carlo.ronconi@mail.polimi.it>
2022-04-01 12:31:55 +0200

    Add javadoc to StudentCounter class

--- src/main/java/it/polimi/ingsw/model/StudentCounter.java
+++ src/main/java/it/polimi/ingsw/model/StudentCounter.java
@@ -4,12 +4,23 @@ import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.Random;
 
+/**
+ * Class StudentCounter counts the number of student tiles for each PawnColor
+ */
 public class StudentCounter {
     private final EnumMap<PawnColor, Integer> map;
 
+    /**
+     * Constructor to create studentCounter with 0 pawns for each color
+     */
     public StudentCounter() {
         this(false);
     }
+
+    /**
+     * Constructor to create studentCounter with 26 pawns for each color
+     * @param full indicates if to initialize the studentCounter with 130 students (26 of each color) or with 0 students
+     */
     public StudentCounter(boolean full) {
         map = new EnumMap<>(PawnColor.class);
         if(!full) { // initialize map with zero students
@@ -25,7 +36,13 @@ public class StudentCounter {
 
     // TODO: create custom exception?
     // TODO: javadoc
-    // moves random pawn between two StudentCounters
+
+    /**
+     * Moves one student of random color pawn (among those with value > 0) from other studentCounter to this
+     * @param other studentCounter to take one student pawn from
+     * @return random color of the moved student
+     * @throws IllegalArgumentException if no color of the other studentCounter has number > 0, would be impossible to move a pawn
+     */
     public PawnColor movePawnFrom(StudentCounter other) throws IllegalArgumentException {
         ArrayList<PawnColor> movableColors = new ArrayList<>();
         for(PawnColor color : PawnColor.values()){
@@ -42,17 +59,31 @@ public class StudentCounter {
         return c;
     }
 
-    // moves pawn of a selected PawnColor between two StudentCounters
+    /**
+     * Moves one student of chosen color pawn (among those with value > 0) from other studentCounter to this
+     * @param other studentCounter to take one student pawn from
+     * @param color chosen color of the student pawn to be moved
+     * @throws IllegalArgumentException if no student of the chosen color is available in other studentCounter to be moved
+     */
     public void movePawnFrom(StudentCounter other, PawnColor color) throws IllegalArgumentException{
         if(other.map.get(color) <= 0) throw new IllegalArgumentException();
         other.map.put(color, other.map.get(color) - 1);
         map.put(color, map.get(color) + 1);
     }
 
+    /**
+     * Counts how many students are present for a given color
+     * @param color color of pawns to be counted
+     * @return number of pawns of chosen color
+     */
     public int count(PawnColor color){
         return map.get(color);
     }
 
+    /**
+     * Counts total amount of students in the studentCounter
+     * @return sum of number of students of each color in PawnColor
+     */
     public int count(){
         int sum = 0;
         for(PawnColor c : PawnColor.values()){
