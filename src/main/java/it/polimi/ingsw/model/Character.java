576cb968161a742653083eb87f8918b01271839a
carloronconi <carlo.ronconi@mail.polimi.it>
2022-04-14 15:35:33 +0200

    add javadoc comments

--- src/main/java/it/polimi/ingsw/model/Character.java
+++ src/main/java/it/polimi/ingsw/model/Character.java
@@ -2,11 +2,22 @@ package it.polimi.ingsw.model;
 
 import java.util.UUID;
 
+/**
+ * Abstract class from which all character cards inherit
+ */
 public abstract class Character {
+    /**
+     * has all attributes common to all character cards:
+     * cost, unique id and boolean representing if any player as ever used the card: if yes, the cost has to be increased by one
+     */
     private final int cost;
     private final UUID id;
     private boolean costIncreased;
 
+    /**
+     * creates character with unique id, costIncreased set to false and cost depending on specific subclass of character
+     * @param cost set by the subclasses
+     */
     public Character(int cost) {
         this.cost = cost;
         id = UUID.randomUUID();
@@ -17,6 +28,9 @@ public abstract class Character {
         return costIncreased;
     }
 
+    /**
+     * costIncreased can just change once from false to true, then never changed again
+     */
     public void assertCostIncreased(){
         costIncreased = true;
     }
--- src/main/java/it/polimi/ingsw/model/Monk.java
+++ src/main/java/it/polimi/ingsw/model/Monk.java
@@ -7,11 +7,22 @@ import it.polimi.ingsw.model.studentmanagers.IslandManager;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
+/**
+ * one of the concrete subclasses of character representing a specific character card
+ */
 public class Monk extends Character{
+    /**
+     * has specific attributes needed to execute its special effect
+     */
     private final CharacterStudentCounter studentCounter;
     private final Bag bag;
     private final IslandManager islandManager;
 
+    /**
+     * creates a monk with cost of 1, draws 4 students from the bag
+     * @param bag bag of the game to draw students from each time students are taken by a player when using effect
+     * @param islandManager needed to move student to island when effect is used
+     */
     public Monk(Bag bag, IslandManager islandManager) {
         super(1);
         this.bag = bag;
@@ -20,6 +31,11 @@ public class Monk extends Character{
         IntStream.range(0,4).forEach(i -> studentCounter.takeStudentFrom(bag));
     }
 
+    /**
+     * special effect with which a player can draw a chosen student from the character card and place it on any island
+     * @param color to choose the student from the monk
+     * @param island destination of the student that will be moved
+     */
     public void useEffect(PawnColor color, UUID island){
         islandManager.moveStudentToIsland(color, island, studentCounter);
         if (!isCostIncreased()) assertCostIncreased();
