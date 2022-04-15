16ec61617668162a2350a4c3372b34cc404f4818
FrancescoArcuri <francesco.arcuri@mail.polimi.it>
2022-04-15 19:21:19 +0200

    Add the internal comments to the checkNewOwner method of the IslandTile class

--- src/main/java/it/polimi/ingsw/model/studentmanagers/IslandTile.java
+++ src/main/java/it/polimi/ingsw/model/studentmanagers/IslandTile.java
@@ -73,6 +73,15 @@ public class IslandTile extends StudentCounter{
      */
     public Player checkNewOwner(ProfessorManager professorManager){
 
+        /**
+         * this method is called by an island and is used to control its new owner.
+         * A Player-integer table is created. You iterate over all the players by setting a number of students
+         * equal to 0 for each one and, for each player, you iterate over the colors of the professor.
+         * The number of students of each color is tracked and, at the end of the iteration,
+         * the total number of students the player has influence on the island under consideration is obtained.
+         * If the player was already the owner of this island, then the influence increases by one.
+         * The Player-Integer pair is inserted into another supporting table.
+         */
         HashMap<Player, Integer> supportNewOwner = new HashMap<>();
         for(Player player : professorManager.playersContained()){
             int numStudents=0;
@@ -87,7 +96,18 @@ public class IslandTile extends StudentCounter{
             supportNewOwner.put(player, numStudents);
         }
 
-
+        /**
+         * in this case a negative value of affected students is set. It iterates over all the players contained
+         * in the support table and the previously set value allows you to define the first player as the player
+         * who has the influence on the island with the relative number of students affected.
+         * This facilitates comparison later. The number of students of the next player will be compared
+         * with that of the previous player and, if it is greater, this player will own (at least for now) the island,
+         * otherwise the previous player will still own it. If, on the other hand, this player should have
+         * a number of students equal to the previous one, then the support player will be set as null
+         * because neither of them will own the island. However, the number of students will be saved
+         * because if the third player has more students than the other previous players,
+         * then this will be set as the player who will own the island.
+         */
         Player supportPlayer = new Player();
         int numInfluenceStudents=-1;
         for(Player player : supportNewOwner.keySet()){
