1d59a4ea567aacdefd882ff5009f54e2764f460e
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2022-04-15 12:30:58 +0200

    feature controller

---
+++ src/main/java/it/polimi/ingsw/controller/MessageHandler.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.model.GameModel;
+
+public class MessageHandler {
+    //an attribute refers to the view - player nickname so I have a getter
+    private int numOfPlayer;
+
+
+    public void getCurrentPlayerID (){}
+    public void sendMessage( /*message, player*/){}
+    public void messageToAll(/*message*/){}
+
+}
