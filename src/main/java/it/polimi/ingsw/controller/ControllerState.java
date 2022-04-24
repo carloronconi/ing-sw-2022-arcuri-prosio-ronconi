ee9ebbc106369f9f5b860343fe60457b4b2fe0e5
carloronconi <carlo.ronconi@mail.polimi.it>
2022-04-24 12:11:44 +0200

    Add handling of condition when user chooses to start over with setup

---
+++ src/main/java/it/polimi/ingsw/controller/ControllerState.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.controller;
+
+public enum ControllerState {
+    INITIAL_SETUP,
+    PLAYING_GAME,
+    GAME_OVER
+}
--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -12,32 +12,38 @@ import java.util.List;
 public class GameController implements EventListener {
     private final CliView view;
     private GameMode gameMode;
+    private ControllerState controllerState;
     private int numOfPlayers;
     //private final GameModel gameModel;
     private final List<String> playerNicknames;
+
 
     public GameController(CliView view) {
         this.view = view;
         //this.gameModel = gameModel;
         playerNicknames = new ArrayList<>();
+        controllerState = ControllerState.INITIAL_SETUP;
     }
 
     public void startSetup(){
 
-        List<String> choices = new ArrayList<>();
-        for (GameMode mode : GameMode.values()) choices.add(mode.name());
-        view.showMultipleChoicePrompt(choices, "Choose game mode", EventType.CHOSE_GAME_MODE);
-        choices = Arrays.asList("2", "3");
-        view.showMultipleChoicePrompt(choices, "Choose number of players", EventType.CHOSE_NUM_OF_PLAYERS);
+        while (controllerState == ControllerState.INITIAL_SETUP){
+            List<String> choices = new ArrayList<>();
+            for (GameMode mode : GameMode.values()) choices.add(mode.name());
+            view.showMultipleChoicePrompt(choices, "Choose game mode", EventType.CHOSE_GAME_MODE);
+            choices = Arrays.asList("2", "3");
+            view.showMultipleChoicePrompt(choices, "Choose number of players", EventType.CHOSE_NUM_OF_PLAYERS);
 
-        for (int i = 0; i < numOfPlayers; i++) {
-            view.showTextInputPrompt("Choose nickname for player number " + i, EventType.CHOSE_NICKNAME);
+            for (int i = 0; i < numOfPlayers; i++) {
+                view.showTextInputPrompt("Choose nickname for player number " + i, EventType.CHOSE_NICKNAME);
+            }
+
+            choices = Arrays.asList("YES", "NO");
+            view.showMultipleChoicePrompt(choices, "Ready to start the game? If not, you will start over with setup", EventType.STARTED_GAME);
         }
 
-        choices = Arrays.asList("YES", "NO");
-        view.showMultipleChoicePrompt(choices, "Ready to start the game", EventType.STARTED_GAME);
-
         //TODO: initialise gameModel accordingly
+
     }
 
     @Override
@@ -49,7 +55,7 @@ public class GameController implements EventListener {
                 break;
             case CHOSE_NICKNAME: playerNicknames.add(data);
                 break;
-            case STARTED_GAME:
+            case STARTED_GAME: if (data.equals("YES")) controllerState = ControllerState.PLAYING_GAME;
                 break;
         }
     }
