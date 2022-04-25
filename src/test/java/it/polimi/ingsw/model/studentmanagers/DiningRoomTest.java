545c45ff706b8d189a9b9ccc576ca02ad5ff30cd
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2022-04-25 13:17:24 +0200

    DiningRoomTest adjourned and debugged test for method fill: movePawn from entrance to dining

--- src/test/java/it/polimi/ingsw/model/studentmanagers/DiningRoomTest.java
+++ src/test/java/it/polimi/ingsw/model/studentmanagers/DiningRoomTest.java
@@ -8,35 +8,49 @@ import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.fail;
 
 public class DiningRoomTest extends StudentCounter{
     private Bag bag;
     private ArrayList<Cloud> clouds;
     private Entrance entrance;
+    private DiningRoom diningRoom;
 
     @BeforeEach
     void setup(){
         bag = new Bag();
         clouds = new ArrayList<>();
-
         IntStream.range(0, 3).forEach(i -> clouds.add(new Cloud(bag)));
-
         entrance = new Entrance(bag, clouds);
+        diningRoom = new DiningRoom(entrance);
+
+        for(PawnColor c : PawnColor.values()) {
+            assertEquals(0, diningRoom.count(c));
+        }
 
     }
 
     @Test
     void fill(){
-        int pawnsInEntranceBefore = entrance.count(PawnColor.BLUE);
-        int pawnsInDiningBefore = this.count(PawnColor.BLUE);
-        movePawnFrom(entrance, PawnColor.BLUE);
 
-        int pawnsInEntranceAfter = entrance.count(PawnColor.BLUE);
-        int pawnsInDiningAfter = this.count(PawnColor.BLUE);
+            int pawnsInEntranceBeforeBLUE = entrance.count(PawnColor.BLUE);
+            int pawnsInDiningBeforeBLUE = diningRoom.count(PawnColor.BLUE);
 
-        assertEquals(pawnsInEntranceBefore-1, pawnsInEntranceAfter);
-        assertEquals(pawnsInDiningBefore+1, pawnsInDiningAfter);
+        try {
+                diningRoom.movePawnFrom(entrance, PawnColor.BLUE);
+
+            }catch(IllegalArgumentException e){
+                fail();
+            }
+            int pawnsInEntranceAfterBLUE = entrance.count(PawnColor.BLUE);
+            int pawnsInDiningAfterBLUE = diningRoom.count(PawnColor.BLUE);
+
+
+        assertEquals(pawnsInEntranceBeforeBLUE - 1, pawnsInEntranceAfterBLUE);
+            assertEquals(pawnsInDiningBeforeBLUE + 1, pawnsInDiningAfterBLUE);
+
+        }
 
     }
 
-}
+
