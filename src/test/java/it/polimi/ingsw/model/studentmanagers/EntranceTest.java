987905b58c7558b051d4f7335e5d5a041480bf1d
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2022-04-25 19:02:28 +0200

    finished test for Entrance method fill

--- src/test/java/it/polimi/ingsw/model/studentmanagers/EntranceTest.java
+++ src/test/java/it/polimi/ingsw/model/studentmanagers/EntranceTest.java
@@ -18,6 +18,11 @@ class EntranceTest extends StudentCounter {
     private Entrance entrance;
     private DiningRoom diningRoom;
     private Cloud cloud;
+    int green;
+    int red;
+    int blue;
+    int yellow;
+    int purple;
 
     @BeforeEach
     void setUp() {
@@ -25,33 +30,51 @@ class EntranceTest extends StudentCounter {
         clouds = new ArrayList<>();
         cloud = new Cloud(bag);
         clouds.add(cloud);
+        int arraySize = clouds.size();
+        assertEquals(1, arraySize);
         cloud.fill(3);
         entrance = new Entrance(bag, clouds);
         assertEquals(7, entrance.count());
+        green = entrance.count(PawnColor.GREEN);
+        red = entrance.count(PawnColor.RED);
+        blue = entrance.count(PawnColor.BLUE);
+        yellow = entrance.count(PawnColor.YELLOW);
+        purple = entrance.count(PawnColor.PURPLE);
         diningRoom = new DiningRoom(entrance);
     }
 
     @Test
     void fill() {
 
-        try {
-            diningRoom.fill(PawnColor.BLUE);
+        assertEquals(7, yellow+green+purple+red+blue);
 
-        } catch (IllegalArgumentException e) {
-            fail();
+        diningRoom.fill(PawnColor.RED);
+        diningRoom.fill(PawnColor.PURPLE);
+        diningRoom.fill(PawnColor.BLUE);
+
+        assertEquals(4, entrance.count());
+        assertEquals(3, diningRoom.count());
+
+            UUID cloudId = clouds.get(0).getId();
+            UUID cloudId2 = cloud.getId();
+
+            assertEquals(cloudId, cloudId2);
+            int pawnCloud = cloud.count();
+            int pawnCloud2 = clouds.get(0).count();
+
+            assertEquals(3, pawnCloud);
+            assertEquals(3, pawnCloud2);
+            assertEquals(pawnCloud, pawnCloud2);
+
+            entrance.fill(clouds.get(0).getId());
+
+
+          assertEquals(7, entrance.count());
+          assertEquals(0, cloud.count());
+
         }
-
-
-        assertEquals(6, entrance.count());
-        assertEquals(1, diningRoom.count());
-
-        entrance.movePawnFrom(clouds.get(0));
-
-        assertEquals(7, entrance.count());
-        assertEquals(2, clouds.get(0).count());
 
     }
 
-}
 
 
