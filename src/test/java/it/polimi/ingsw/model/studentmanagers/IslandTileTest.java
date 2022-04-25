82a914e670f6279b51bd1eb17ece612cecb50189
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2022-04-25 13:35:20 +0200

    IslandTile tested method moveAllPawnsFrom

--- src/test/java/it/polimi/ingsw/model/studentmanagers/IslandTileTest.java
+++ src/test/java/it/polimi/ingsw/model/studentmanagers/IslandTileTest.java
@@ -12,15 +12,16 @@ import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class IslandTileTest extends StudentCounter implements Identifiable {
+class IslandTileTest extends StudentCounter {
     private Bag bag;
     private List<Cloud> clouds;
-    Entrance entrance;
-    IslandTile curr;
-    IslandTile next;
-    IslandTile prev;
-    UUID tileId;
-    int size =1;
+    private Entrance entrance;
+    private IslandTile curr;
+    private IslandTile next;
+    int pawnCurr;
+    int pawnNext;
+    int pawnCurrAfter;
+    private int size =1;
 
     @BeforeEach
     void setUp() {
@@ -31,23 +32,32 @@ class IslandTileTest extends StudentCounter implements Identifiable {
         entrance = new Entrance(bag,clouds);
         curr = new IslandTile();
         next = new IslandTile();
-        prev = new IslandTile();
+        pawnCurr = curr.count();
+        pawnNext = next.count();
+
     }
 
     //TODO: implement tests for all methods when IslandTile class completed
 
     @Test
-    void  moveAllPawnsFrom(IslandTile next){
+    void  moveAllPawnsFrom(){
         size++;
         for (PawnColor c: PawnColor.values()) {
             for (int i = 0; i < next.count(c); i++) {
-                movePawnFrom(next,c);
+                curr.movePawnFrom(next,c);
+                pawnCurrAfter = curr.count();
             }
         }
 
         assertEquals(2, size);
+        assertEquals(pawnCurr+pawnNext, pawnCurrAfter);
 
     }
 
-    public UUID getId() {return tileId;}
+    @Test
+    void checkNewOwner(){
+
+    }
+
+
 }
