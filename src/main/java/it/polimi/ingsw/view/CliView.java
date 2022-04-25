02c4bee1b7ee1807b686e9724fd51942635acd75
carloronconi <carlo.ronconi@mail.polimi.it>
2022-04-25 10:26:35 +0200

    Make view a listener of model events and first mockup methods to support it

--- src/main/java/it/polimi/ingsw/view/CliView.java
+++ src/main/java/it/polimi/ingsw/view/CliView.java
@@ -1,11 +1,13 @@
 package it.polimi.ingsw.view;
 
+import it.polimi.ingsw.EventListener;
 import it.polimi.ingsw.EventManager;
+import it.polimi.ingsw.model.ModelEventType;
 
 import java.util.List;
 import java.util.Scanner;
 
-public class CliView {
+public class CliView implements EventListener<ModelEventType> {
     private final EventManager<ViewEventType> eventManager;
 
     public CliView(EventManager<ViewEventType> eventManager) {
@@ -28,5 +30,18 @@ public class CliView {
         Scanner s = new Scanner(System.in);
         String text = s.nextLine();
         eventManager.notify(viewEventType, text);
+    }
+
+    private void drawMainView(String data){
+        //TODO: should draw the main view of the model using getters to see its state
+        System.out.println(data);
+    }
+
+    @Override
+    public void update(ModelEventType modelEventType, String data) {
+        switch (modelEventType) {
+            case INITIATED_MODEL: drawMainView(data);
+                break;
+        }
     }
 }
