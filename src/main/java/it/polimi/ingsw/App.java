9c8a1f1b4ae9e2eba319a0a27c5e79a0890b9a48
carloronconi <carlo.ronconi@mail.polimi.it>
2022-04-29 16:46:25 +0200

    Remove unnecessary classes ModelEvent, Parsable, ViewGameInitializationEvent, ViewInGameEvent, CliView used to create a first prototype without socket

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,9 +1,5 @@
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.controller.GameController;
-import it.polimi.ingsw.model.ModelEvent;
-import it.polimi.ingsw.view.CliView;
-import it.polimi.ingsw.view.ViewGameInitializationEvent;
 
 /**
  * Hello world!
@@ -13,17 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        EventManager<ViewGameInitializationEvent> viewEventManager = new EventManager<>(ViewGameInitializationEvent.class);
-        CliView cliView = new CliView(viewEventManager);
-        GameController gameController = new GameController(cliView);
-        //for now game controller subscribes for all types of event
-        for (ViewGameInitializationEvent e : ViewGameInitializationEvent.values()){
-            viewEventManager.subscribe(e,gameController);
-        }
-        EventManager<ModelEvent> modelEventManager = new EventManager<>(ModelEvent.class);
-        for (ModelEvent e : ModelEvent.values()){
-            modelEventManager.subscribe(e,cliView);
-        }
-        gameController.startGame(modelEventManager);
+
     }
 }
