48cefd5f15cb99bf13ebdfe51534a483d67a2ec2
FrancescoArcuri <francesco.arcuri@mail.polimi.it>
2022-04-29 11:35:19 +0200

    Add MoveStudent class into package networkmessages

---
+++ src/main/java/it/polimi/ingsw/networkmessages/MoveStudent.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.networkmessages;
+
+import it.polimi.ingsw.model.PawnColor;
+import it.polimi.ingsw.model.Player;
+
+import java.util.EnumMap;
+
+public class MoveStudent {
+}
