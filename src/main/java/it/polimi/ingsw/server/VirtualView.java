2977c32b4bd3f9d7156bed5908bb89455e1a1424
carloronconi <carlo.ronconi@mail.polimi.it>
2022-04-29 16:44:04 +0200

    Add new VirtualView interface

---
+++ src/main/java/it/polimi/ingsw/server/VirtualView.java
@@ -0,0 +1,65 @@
+package it.polimi.ingsw.server;
+
+import it.polimi.ingsw.ViewInterface;
+
+public class VirtualView implements ViewInterface {
+    @Override
+    public void sendAcknowledgement() {
+
+    }
+
+    @Override
+    public void askPlayAgain() {
+
+    }
+
+    @Override
+    public void chooseCharacter() {
+
+    }
+
+    @Override
+    public void chooseCloud() {
+
+    }
+
+    @Override
+    public void getAssistantCard() {
+
+    }
+
+    @Override
+    public void getNickname() {
+
+    }
+
+    @Override
+    public void getPreferences() {
+
+    }
+
+    @Override
+    public void invalidCharacterChoice() {
+
+    }
+
+    @Override
+    public void invalidMNMove() {
+
+    }
+
+    @Override
+    public void invalidNickname() {
+
+    }
+
+    @Override
+    public void moveMotherNature() {
+
+    }
+
+    @Override
+    public void moveStudent() {
+
+    }
+}
