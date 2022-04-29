a699f53354811c653a2edac8b9741d59ebbc767f
carloronconi <carlo.ronconi@mail.polimi.it>
2022-04-29 16:45:45 +0200

    Remove unnecessary classes ModelEvent, Parsable, ViewGameInitializationEvent, ViewInGameEvent, CliView used to create a first prototype without socket

--- src/main/java/it/polimi/ingsw/model/ModelEvent.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.model;
-
-/**
- * types of event in the model to which the view has to react
- */
-public enum ModelEvent {
-    INITIATED_MODEL
-}
--- src/main/java/it/polimi/ingsw/view/Parsable.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.view;
-
-/**
- * needed to pass lambda expression to showTextInputPrompt method in CliView class
- */
-public interface Parsable {
-    String parse(String text);
-}
