ad68955fec3261900a585a08d35c7559c00ccde6
carloronconi <carlo.ronconi@mail.polimi.it>
2022-04-29 15:21:59 +0200

    Add view interface to be implemented by virtual view and client view

---
+++ src/main/java/it/polimi/ingsw/ViewInterface.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw;
+
+public interface ViewInterface {
+
+    void sendAcknowledgement();
+
+    void askPlayAgain();
+
+    void chooseCharacter();
+
+    void chooseCloud();
+
+    void getAssistantCard();
+
+    void getNickname();
+
+    void getPreferences();
+
+    void invalidCharacterChoice();
+
+    void invalidMNMove();
+
+    void invalidNickname();
+
+    void moveMotherNature();
+
+    void moveStudent();
+
+}
