0562928ac70f73aabebb3d73a7e2fd97274e36d9
carloronconi <carlo.ronconi@mail.polimi.it>
2022-04-29 12:54:56 +0200

    Add attribute to MovedStudent message

--- src/main/java/it/polimi/ingsw/networkmessages/MovedStudent.java
+++ src/main/java/it/polimi/ingsw/networkmessages/MovedStudent.java
@@ -2,15 +2,22 @@ package it.polimi.ingsw.networkmessages;
 
 import it.polimi.ingsw.model.PawnColor;
 
+import java.util.UUID;
+
 public class MovedStudent {
     private final PawnColor color;
+    private final UUID islandId;
 
-    public MovedStudent(PawnColor color){
+    public MovedStudent(PawnColor color, UUID islandId){
         this.color=color;
+        this.islandId = islandId; //null if the student has to be moved to the entrance
     }
 
     public PawnColor getColor(){
         return color;
     }
 
+    public UUID getIslandId() {
+        return islandId;
+    }
 }
