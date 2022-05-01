eefe850cae6bb09fddd3bc351b31dfa6fd0e83a3
carloronconi <carlo.ronconi@mail.polimi.it>
2022-05-01 17:16:46 +0200

    Create constructor and method reorderPlanning

--- src/main/java/it/polimi/ingsw/controller/TurnController.java
+++ src/main/java/it/polimi/ingsw/controller/TurnController.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.controller;
 
 import it.polimi.ingsw.EventListener;
+import it.polimi.ingsw.model.ConverterUtility;
 import it.polimi.ingsw.model.GameModel;
 import it.polimi.ingsw.networkmessages.modelevents.ModelEvent;
 import it.polimi.ingsw.networkmessages.viewevents.GameViewEvent;
@@ -9,22 +10,43 @@ import java.io.InvalidObjectException;
 import java.util.*;
 
 public class TurnController implements EventListener<GameViewEvent> {
-    private List<UUID> playerIds;
+    private List<UUID> planningPlayerOrder;
+    private List<UUID> actionPlayerOrder;
     private final GameModel gameModel;
 
     public TurnController(List<UUID> playerIds, GameModel gameModel) {
-        this.playerIds = playerIds;
-        Collections.shuffle(playerIds);
         this.gameModel = gameModel;
+        planningPlayerOrder = playerIds;
+        actionPlayerOrder = playerIds;
+
+        Random random = new Random();
+        int index = random.nextInt(planningPlayerOrder.size());
+        UUID firstPlayer = planningPlayerOrder.get(index);
+        reorderPlanning(firstPlayer);
+
     }
 
-    public void startRound() throws NoSuchFieldException {
+    private void reorderPlanning(UUID firstPlayer){
+        int first = planningPlayerOrder.indexOf(firstPlayer);
+        ArrayList<UUID> list = new ArrayList<>();
+        list.add(firstPlayer);
+        for (int i = first; i < planningPlayerOrder.size(); i++) {
+            list.add(planningPlayerOrder.get(i));
+        }
+        for (int i = 0; i < first; i++) {
+            list.add(planningPlayerOrder.get(i));
+        }
+    }
+
+    public void startRound() {
         startPlanningPhase();
         startActionPhase();
     }
 
-    private void startPlanningPhase() throws NoSuchFieldException {
+    private void startPlanningPhase() {
         gameModel.fillAllClouds();
+        gameModel.clearPlayedAssistantCards();
+
         for (UUID player : playerIds){
             ArrayList<Integer> choicesInt = gameModel.getDeck(player);
             ArrayList<String> choices = new ArrayList<>();
