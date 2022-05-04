9574df974d07ee55ef1e9dbdeaef8dde6d3dc500
carloronconi <carlo.ronconi@mail.polimi.it>
2022-05-04 19:30:30 +0200

    Add interfaces for characters to set the attribute needed for each specific useEffect() method

---
+++ src/main/java/it/polimi/ingsw/model/charactercards/effectarguments/EffectWithColor.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model.charactercards.effectarguments;
+
+import it.polimi.ingsw.model.PawnColor;
+
+public interface EffectWithColor {
+    void setEffectColor(PawnColor color);
+}
---
+++ src/main/java/it/polimi/ingsw/model/charactercards/effectarguments/EffectWithPlayer.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model.charactercards.effectarguments;
+
+import java.util.UUID;
+
+public interface EffectWithPlayer {
+    void setEffectPlayer(UUID player);
+}
