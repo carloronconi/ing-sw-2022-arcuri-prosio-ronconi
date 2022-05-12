27357b42833a80f390c342d701bdb4ba3a2d79ed
FrancescoArcuri <francesco.arcuri@mail.polimi.it>
2022-05-12 11:02:05 +0200

    Add partial CheeseMerchantTest class

--- src/test/java/it/polimi/ingsw/model/charactercards/CheeseMerchantTest.java
+++ src/test/java/it/polimi/ingsw/model/charactercards/CheeseMerchantTest.java
@@ -19,5 +19,15 @@ class CheeseMerchantTest {
         assertFalse(cheeseMerchant.isCostIncreased());
     }
 
+    /*
+    - creare due nickname e metterli in una lista
+    - istanziare gamemodel passando la lista con i due nickname
+    - controllare che entrambi i giocatori abbiano almeno uno studente dello stesso colore. se si, spostare
+        lo studente dall'entrata alla diningRoom del primo player
+    - chiamare il metodo "updateProfessorManager" così da far controllare il professore di quel colore al player
+    - spostare lo studente dall'entrata alla diningRoom del secondo studente
+    - chiamare l'effetto di cheeseMerchant, passando il secondo player, per testarne il funzionamento
+     */
+
 
 }
