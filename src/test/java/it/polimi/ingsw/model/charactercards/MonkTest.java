0ee02448bcb98063a73aff317606398a2337a519
FrancescoArcuri <francesco.arcuri@mail.polimi.it>
2022-05-12 10:54:11 +0200

    Add MonkTest class

--- src/test/java/it/polimi/ingsw/model/charactercards/MonkTest.java
+++ src/test/java/it/polimi/ingsw/model/charactercards/MonkTest.java
@@ -29,8 +29,11 @@ public class MonkTest {
         assertEquals(116, bag.count());
     }
 
+    /**
+     * this method verifies that the monk effect works correctly
+     */
     @Test
-    public void useEffect() throws NoSuchFieldException {
+    public void useEffect() {
 
         IslandTile islandTile = islandManager.getIsland(1);
 
@@ -39,7 +42,11 @@ public class MonkTest {
                 int numStudents=islandTile.count(color);
                 monk.setEffectColor(color);
                 monk.setEffectIsland(islandTile.getId());
-                monk.useEffect();
+                try {
+                    monk.useEffect();
+                } catch (NoSuchFieldException e) {
+                    fail();
+                }
                 assertEquals(115, bag.count());
                 assertEquals(numStudents+1, islandTile.count(color));
                 break;
@@ -48,14 +55,21 @@ public class MonkTest {
 
     }
 
+    /**
+     * this method verifies that the island on which the student is to be placed and the color of the student are set
+     */
     @Test (expected = IllegalStateException.class)
-    public void useEffect2() throws NoSuchFieldException{
+    public void useEffect2() {
         IslandTile islandTile = islandManager.getIsland(1);
 
         for(PawnColor color : PawnColor.values()){
             if(monk.isColorContained(color)){
                 int numStudents=islandTile.count(color);
-                monk.useEffect();
+                try {
+                    monk.useEffect();
+                } catch (NoSuchFieldException e) {
+                    fail();
+                }
                 assertEquals(115, bag.count());
                 assertEquals(numStudents+1, islandTile.count(color));
                 break;
