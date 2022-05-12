481ff78b5724298252bd186e045ae4aa7fc6b22b
FrancescoArcuri <francesco.arcuri@mail.polimi.it>
2022-05-12 11:00:27 +0200

    Add MusicianTest class

--- src/test/java/it/polimi/ingsw/model/charactercards/MusicianTest.java
+++ src/test/java/it/polimi/ingsw/model/charactercards/MusicianTest.java
@@ -11,22 +11,24 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MusicianTest {
     private Musician musician;
-    private Entrance entrance;
-    private DiningRoom diningRoom;
+    private Player player;
+    private PawnColor colorTake;
+    private PawnColor colorGive;
+    private int numStudentsTake;
+    private int numStudentsGive;
 
     @Before
     public void setUp(){
         Bag bag = new Bag();
         List<Cloud> clouds = new ArrayList<>();
-        entrance = new Entrance(bag, clouds, 7);
-        diningRoom = new DiningRoom(entrance);
-        Player player = new Player(entrance, diningRoom, "testname");
+        Entrance entrance = new Entrance(bag, clouds, 7);
+        DiningRoom diningRoom = new DiningRoom(entrance);
+        player = new Player(entrance, diningRoom, "testname");
         List<Player> players = new ArrayList<>();
 
         players.add(player);
@@ -37,44 +39,47 @@ public class MusicianTest {
         assertEquals(1, players.size());
         assertEquals(1, musician.getCost());
         assertFalse(musician.isCostIncreased());
+        assertEquals(AvailableCharacter.MUSICIAN, musician.getValue());
 
 
-        /*
-        ArrayList<PawnColor> movableColors = new ArrayList<>();
-        for(PawnColor color : PawnColor.values()){
-            if(player.getEntrance().get(color) > 0){
-                movableColors.add(color);
-            }
-        }
-        if(movableColors.size() == 0) throw new IllegalArgumentException();
-
-        Random rand = new Random();
-        int i = rand.nextInt(movableColors.size());
-        PawnColor c = movableColors.get(i);
-        movePawnFrom(other, c);
-         */
-
-
-
-
-        for(Player p : players){
-            int numTransferedStudents=0;
-            boolean numReached=false;
-            for(PawnColor pawnColor : PawnColor.values()){
-                if(p.getEntrance().count(pawnColor)>1 && numTransferedStudents<2){
-                    p.getDiningRoom().fill(pawnColor);
-                    numTransferedStudents++;
-                }
+        for(PawnColor c : PawnColor.values()){
+            if(player.getEntrance().count(c)>0){
+                player.getDiningRoom().fill(c);
+                colorTake=c;
+                numStudentsTake=player.getEntrance().count(c);
+                break;
             }
         }
 
+        for(PawnColor c : PawnColor.values()){
+            if(player.getEntrance().count(c)>0 && colorTake!=c){
+                colorGive=c;
+                numStudentsGive=player.getEntrance().count(colorGive);
+                break;
+            }
+        }
 
+        musician.setupColorSwaps(colorGive, colorTake);
 
     }
 
+    /**
+     * this method tests that the musician effect works correctly
+     */
     @Test
     public void useEffect(){
+        musician.setEffectPlayer(player.getId());
 
+        try {
+            musician.useEffect();
+        } catch (NoSuchFieldException e) {
+            fail();
+        }
+
+        assertEquals(2, musician.getCost());
+        assertEquals(numStudentsTake+1, player.getEntrance().count(colorTake));
+        assertEquals(numStudentsGive-1, player.getEntrance().count(colorGive));
+        assertTrue(musician.isCostIncreased());
     }
 
 }
