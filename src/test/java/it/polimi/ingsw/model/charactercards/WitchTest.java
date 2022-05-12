538e3f78361fe3fdfb3d094498d535c13f2dafcd
FrancescoArcuri <francesco.arcuri@mail.polimi.it>
2022-05-12 11:01:20 +0200

    Add WitchTest class

--- src/test/java/it/polimi/ingsw/model/charactercards/WitchTest.java
+++ src/test/java/it/polimi/ingsw/model/charactercards/WitchTest.java
@@ -21,6 +21,7 @@ public class WitchTest {
 
         assertEquals(2, witch.getCost());
         assertFalse(witch.isCostIncreased());
+        assertEquals(AvailableCharacter.WITCH, witch.getValue());
 
         for(int i =0; i< islandManager.countIslands(); i++){
             assertFalse(islandManager.getIsland(i).ban);
@@ -28,39 +29,100 @@ public class WitchTest {
 
     }
 
+    /**
+     * this method verifies that the witch effect works correctly
+     */
     @Test
-    public void useEffect() throws NoSuchFieldException {
+    public void useEffect(){
         witch.setEffectIsland(islandManager.getIsland(2).getId());
-        witch.useEffect();
+        try {
+            witch.useEffect();
+        } catch (NoSuchFieldException e) {
+            fail();
+        }
         assertTrue(islandManager.getIsland(2).ban);
         assertTrue(witch.isCostIncreased());
         assertEquals(3, witch.getCost());
     }
 
 
+    /**
+     * this method tests that the maximum number of bans that can be assigned is 4
+     */
     @Test (expected = IllegalStateException.class)
-    public void useEffect2() throws NoSuchFieldException{
+    public void useEffect2(){
         witch.setEffectIsland(islandManager.getIsland(3).getId());
-        witch.useEffect();
+        try {
+            witch.useEffect();
+        } catch (NoSuchFieldException e) {
+            fail();
+        }
         witch.setEffectIsland(islandManager.getIsland(4).getId());
-        witch.useEffect();
+        try {
+            witch.useEffect();
+        } catch (NoSuchFieldException e) {
+            fail();
+        }
         witch.setEffectIsland(islandManager.getIsland(5).getId());
-        witch.useEffect();
+        try {
+            witch.useEffect();
+        } catch (NoSuchFieldException e) {
+            fail();
+        }
         witch.setEffectIsland(islandManager.getIsland(7).getId());
-        witch.useEffect();
+        try {
+            witch.useEffect();
+        } catch (NoSuchFieldException e) {
+            fail();
+        }
         witch.setEffectIsland(islandManager.getIsland(8).getId());
-        witch.useEffect();
+        try {
+            witch.useEffect();
+        } catch (NoSuchFieldException e) {
+            fail();
+        }
+    }
+
+    /**
+     * this method tests that the exception is thrown when the island to which the ban is to be assigned is not set
+     */
+    @Test (expected = IllegalStateException.class)
+    public void useEffect3() {
+        try {
+            witch.useEffect();
+        } catch (NoSuchFieldException e) {
+            fail();
+        }
     }
 
 
+    /**
+     * this method increases bans after motherNature arrives on the island which owns one
+     */
     @Test
-    public void increaseAvailableBans() throws NoSuchFieldException {
-        islandManager.moveMotherNature(2);
-        assertFalse(islandManager.getIsland(2).ban);
+    public void increaseAvailableBans(){
+        witch.setEffectIsland(islandManager.getIsland(8).getId());
+        try {
+            witch.useEffect();
+        } catch (NoSuchFieldException e) {
+            fail();
+        }
+
+        assertTrue(islandManager.getIsland(8).ban);
+
+
+        try {
+            islandManager.moveMotherNature(8);
+        } catch (NoSuchFieldException e) {
+            fail();
+        }
+        assertFalse(islandManager.getIsland(8).ban);
 
     }
 
-
+    /**
+     * this method throws an exception when the island passed as a parameter is not the correct one
+     */
     @Test (expected = IllegalArgumentException.class)
     public void increaseAvailableBans2(){
         IslandManager islandManager = null;
