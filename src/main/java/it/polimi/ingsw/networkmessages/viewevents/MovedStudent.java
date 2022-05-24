1613bfdfb3ecd53a93cb5e2df734429e762a4895
carloronconi <carlo.ronconi@mail.polimi.it>
2022-05-24 17:08:13 +0200

    Fix bug: before it asked to move student just once, now 3 or 4 times based on game mode

--- src/main/java/it/polimi/ingsw/networkmessages/viewevents/MovedStudent.java
+++ src/main/java/it/polimi/ingsw/networkmessages/viewevents/MovedStudent.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.networkmessages.viewevents;
 
+import it.polimi.ingsw.controller.GameMode;
 import it.polimi.ingsw.model.PawnColor;
 import it.polimi.ingsw.server.ClientHandler;
 import it.polimi.ingsw.server.VirtualView;
@@ -11,6 +12,8 @@ import java.util.UUID;
 public class MovedStudent implements Serializable, GameViewEvent {
     private final PawnColor color;
     private final UUID islandId;
+
+    private static int colorsChosen = 0;
 
     public MovedStudent(PawnColor color, UUID islandId){
         this.color=color;
@@ -29,7 +32,16 @@ public class MovedStudent implements Serializable, GameViewEvent {
     public void processMessage(VirtualView virtualView) throws InvalidObjectException {
         //TODO: react if move is invalid with new message (never created class)
         virtualView.notifyController(this);
-        virtualView.moveMotherNature();
+
+        int totColorsToChoose = virtualView.getGameMode()== GameMode.EASY? 3 : 4;
+        totColorsToChoose--;
+        if (colorsChosen<totColorsToChoose){
+            colorsChosen++;
+            virtualView.moveStudent();
+        } else {
+            colorsChosen = 0;
+            virtualView.moveMotherNature();
+        }
 
     }
 }
