f26930bd092c7a7e8d7d51e8ad45cb5f6d5d4412
carloronconi <carlo.ronconi@mail.polimi.it>
2022-05-24 15:43:24 +0200

    Add processMessage implementation

--- src/main/java/it/polimi/ingsw/networkmessages/viewevents/ChosenCloud.java
+++ src/main/java/it/polimi/ingsw/networkmessages/viewevents/ChosenCloud.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.networkmessages.viewevents;
 
+import it.polimi.ingsw.controller.GameMode;
+import it.polimi.ingsw.controller.TurnState;
 import it.polimi.ingsw.server.ClientHandler;
 import it.polimi.ingsw.server.VirtualView;
 
@@ -19,6 +21,30 @@ public class ChosenCloud implements Serializable, GameViewEvent {
 
     @Override
     public void processMessage(VirtualView virtualView) throws InvalidObjectException {
+        virtualView.notifyController(this);
 
+        synchronized (SetAssistantCard.class){
+            //turn ends after setting the assistant card
+            virtualView.playerFinishedTurn();
+            SetAssistantCard.class.notifyAll();
+        }
+
+        synchronized (ChosenCloud.class){
+            ChosenCloud.class.notifyAll();
+        }
+        if(!(virtualView.isItMyTurn() && virtualView.getTurnControllerState() == TurnState.PLANNING)){
+            synchronized (ChosenCloud.class){
+                while (!virtualView.isItMyTurn()) {
+                    try {
+                        ChosenCloud.class.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        System.out.println("thread " + virtualView.getThisInstanceNumber() + " starts its turn in planning phase");
+        virtualView.getAssistantCard();
     }
 }
