33e879fab2344ac17af22b504fa06d63f9a71343
FrancescoArcuri <francesco.arcuri@mail.polimi.it>
2022-05-27 16:55:11 +0200

    Add tower colors in the Color class

--- src/main/java/it/polimi/ingsw/cliview/Color.java
+++ src/main/java/it/polimi/ingsw/cliview/Color.java
@@ -1,9 +1,12 @@
 package it.polimi.ingsw.cliview;
 
 public enum Color {
+    ANSI_BLACK("\u001B[30m"),
+    ANSI_GREY("\u001B[90m"),
+    ANSI_WHITE("\u001B[97m"),
     ANSI_RED("\u001B[31m"),
     ANSI_GREEN("\u001B[32m"),
-    ANSI_YELLOW("\u001B[33m"),
+    ANSI_YELLOW("\u001B[93m"),
     ANSI_BLUE("\u001B[34m"),
     ANSI_PURPLE("\u001B[35m"),
     RESET("\u001B[0m");
