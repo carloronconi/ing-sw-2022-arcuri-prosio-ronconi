c7580b78fe0e4cb6b612a3357af3cc6b4358837a
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2022-05-27 15:53:42 +0200

    Added class that does not extend from Application but calls method "lauch" - step necessary to work with Maven and JavaFX in this project

---
+++ src/main/java/it/polimi/ingsw/client/ClientMainGui.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.client;
+
+import javafx.application.Application;
+
+public class ClientMainGui {
+    public static void main(String[] args){
+        Application.launch(ClientGUIFirst.class);
+    }
+}
