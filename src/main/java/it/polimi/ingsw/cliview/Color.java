534e22b5c90f401ad44569ff1c1b16d4a434ceff
FrancescoArcuri <francesco.arcuri@mail.polimi.it>
2022-05-28 17:52:53 +0200

    Add methods pawnColorConverter and towerColorConverter

--- src/main/java/it/polimi/ingsw/cliview/Color.java
+++ src/main/java/it/polimi/ingsw/cliview/Color.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.cliview;
 
+import it.polimi.ingsw.model.PawnColor;
+import it.polimi.ingsw.model.TowerColor;
+
 public enum Color {
     ANSI_BLACK("\u001B[30m"),
     ANSI_GREY("\u001B[90m"),
@@ -26,6 +29,34 @@ public enum Color {
     public String getEscape()
     {
         return escape;
+    }
+
+    public static Color pawnColorConverter(PawnColor color){
+        switch (color){
+            case RED:
+                return Color.ANSI_RED;
+            case GREEN:
+                return Color.ANSI_GREEN;
+            case BLUE:
+                return Color.ANSI_BLUE;
+            case YELLOW:
+                return Color.ANSI_YELLOW;
+            case PURPLE:
+                return Color.ANSI_PURPLE;
+        }
+        return Color.RESET;
+    }
+
+    public static Color towerColorConverter(TowerColor color){
+        switch (color){
+            case WHITE:
+                return Color.ANSI_WHITE;
+            case BLACK:
+                return Color.ANSI_BLACK;
+            case GREY:
+                return Color.ANSI_GREY;
+        }
+        return Color.RESET;
     }
 
 
