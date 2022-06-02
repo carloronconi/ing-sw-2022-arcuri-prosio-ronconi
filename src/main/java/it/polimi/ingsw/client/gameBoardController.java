1571b8eec7931f9b7de77f10a7958c84ad473ca4
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2022-06-02 23:55:29 +0200

    Added class specifically to control permissible moves in the game (entrance to island, entrance to dining). Those methods are to develop tomorrow

---
+++ src/main/java/it/polimi/ingsw/client/gameBoardController.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.client;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+
+import java.awt.*;
+
+public class gameBoardController {
+
+    @FXML
+    GridPane gridPane;
+    @FXML
+    ColumnConstraints column1;
+    @FXML
+    RowConstraints row1;
+
+    /*TODO: finish adding an id to remaining gridPanes (the schools) and add Students to entrance of a board. Try to set movements of those
+    students (aka entrance to islands and entrance to dining.
+    For now no game rules, just allowed movements.
+     */
+
+
+
+
+}
