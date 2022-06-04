d845e5a3508818f8d93bfe4b3d2ac636e3447385
FrancescoArcuri <francesco.arcuri@mail.polimi.it>
2022-06-04 16:30:14 +0200

    Modify test to the EntranceTest class

--- src/test/java/it/polimi/ingsw/model/studentmanagers/EntranceTest.java
+++ src/test/java/it/polimi/ingsw/model/studentmanagers/EntranceTest.java
@@ -1,70 +1,55 @@
 package it.polimi.ingsw.model.studentmanagers;
 
-import it.polimi.ingsw.model.ConverterUtility;
-import it.polimi.ingsw.model.Identifiable;
 import it.polimi.ingsw.model.PawnColor;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.UUID;
-import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class EntranceTest extends StudentCounter {
+public class EntranceTest extends StudentCounter {
     private ArrayList<Cloud> clouds;
-    private Bag bag;
     private Entrance entrance;
     private DiningRoom diningRoom;
-    private Cloud cloud;
-    int green;
-    int red;
-    int blue;
-    int yellow;
-    int purple;
 
-    @BeforeEach
-    void setUp() {
-        bag = new Bag();
+    @Before
+    public void setUp() {
+        Bag bag = new Bag();
+        Cloud cloud = new Cloud(bag);
         clouds = new ArrayList<>();
-        cloud = new Cloud(bag);
         clouds.add(cloud);
-        int arraySize = clouds.size();
-        assertEquals(1, arraySize);
+
         cloud.fill(3);
+        assertEquals(3, cloud.count());
+
         entrance = new Entrance(bag, clouds, 7);
         assertEquals(7, entrance.count());
-        green = entrance.count(PawnColor.GREEN);
-        red = entrance.count(PawnColor.RED);
-        blue = entrance.count(PawnColor.BLUE);
-        yellow = entrance.count(PawnColor.YELLOW);
-        purple = entrance.count(PawnColor.PURPLE);
+
         diningRoom = new DiningRoom(entrance);
+        assertEquals(0, diningRoom.count());
+
     }
 
+    /**
+     * this method verifies that the filling of the entrance from the cloud works correctly
+     */
     @Test
-    void fill() {
+    public void fill() {
 
-        assertEquals(7, yellow+green+purple+red+blue);
-
-        diningRoom.fill(PawnColor.RED);
-        diningRoom.fill(PawnColor.PURPLE);
-        diningRoom.fill(PawnColor.BLUE);
+        int studentsNum = 0;
+        for (PawnColor color : PawnColor.values()){
+            if (entrance.count(color)>0){
+                diningRoom.fill(color);
+                studentsNum++;
+            }
+            if (studentsNum == 3){
+                break;
+            }
+        }
 
         assertEquals(4, entrance.count());
         assertEquals(3, diningRoom.count());
-
-            UUID cloudId = clouds.get(0).getId();
-            UUID cloudId2 = cloud.getId();
-
-            assertEquals(cloudId, cloudId2);
-            int pawnCloud = cloud.count();
-            int pawnCloud2 = clouds.get(0).count();
-
-            assertEquals(3, pawnCloud);
-            assertEquals(3, pawnCloud2);
-            assertEquals(pawnCloud, pawnCloud2);
 
         try {
             entrance.fill(clouds.get(0).getId());
@@ -72,9 +57,9 @@ class EntranceTest extends StudentCounter {
             e.printStackTrace();
         }
 
-
+        assertEquals(0, clouds.get(0).count());
         assertEquals(7, entrance.count());
-          assertEquals(0, cloud.count());
+        assertEquals(3, diningRoom.count());
 
         }
 
