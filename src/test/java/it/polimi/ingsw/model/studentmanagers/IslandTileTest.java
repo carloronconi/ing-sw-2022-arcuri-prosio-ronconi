40ba875fa0ab3c41589dd421c84244dc8b258837
FrancescoArcuri <francesco.arcuri@mail.polimi.it>
2022-06-04 16:32:09 +0200

    Modify test to the IslandTileTest class

--- src/test/java/it/polimi/ingsw/model/studentmanagers/IslandTileTest.java
+++ src/test/java/it/polimi/ingsw/model/studentmanagers/IslandTileTest.java
@@ -1,94 +1,42 @@
 package it.polimi.ingsw.model.studentmanagers;
 
-import it.polimi.ingsw.model.Identifiable;
-import it.polimi.ingsw.model.PawnColor;
-import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.ProfessorManager;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.IntStream;
-
-import static it.polimi.ingsw.model.PawnColor.BLUE;
-import static it.polimi.ingsw.model.PawnColor.RED;
 import static org.junit.jupiter.api.Assertions.*;
 
-class IslandTileTest extends StudentCounter {
-    private Bag bag;
-    private List<Cloud> clouds;
-    private Entrance entrance;
-    private IslandTile curr;
-    private IslandTile next;
-    int pawnCurr;
-    int pawnNext;
-    int pawnCurrAfter;
-    private int size =1;
+public class IslandTileTest extends StudentCounter {
 
-    private Player owner;
-    private Player otherPlayer;
-    private DiningRoom diningRoom;
-    private ProfessorManager professorManager;
-    private Player newOwner;
+    private IslandManager islandManager;
 
-    @BeforeEach
-    void setUp() {
-        bag = new Bag();
-        clouds = new ArrayList<>();
-        IntStream.range(0,3).forEach(i -> clouds.add(new Cloud(bag)));
+    @Before
+    public void setUp() {
+        Bag bag = new Bag();
+        ProfessorManager professorManager = new ProfessorManager();
+        islandManager = new IslandManager(bag, professorManager);
 
-        entrance = new Entrance(bag,clouds, 7);
-        curr = new IslandTile();
-        next = new IslandTile();
-        pawnCurr = curr.count();
-        pawnNext = next.count();
-
-        //checkNewOwner setup
-        diningRoom = new DiningRoom(entrance);
-        owner = new Player(entrance, diningRoom, "testname1");
-        otherPlayer = new Player(entrance, diningRoom, "testname2");
-        professorManager = new ProfessorManager();
-        professorManager.setProfessorOwner(BLUE, owner);
-        professorManager.setProfessorOwner(RED, owner);
-        professorManager.setProfessorOwner(PawnColor.GREEN, otherPlayer);
-
-    }
-
-    //TODO: implement tests for all methods when IslandTile class completed
-
-    @Test
-    void  moveAllPawnsFrom(){
-        size++;
-        for (PawnColor c: PawnColor.values()) {
-            for (int i = 0; i < next.count(c); i++) {
-                curr.movePawnFrom(next,c);
-                pawnCurrAfter = curr.count();
+        assertEquals(12, islandManager.countIslands());
+        for (IslandTile islandTile : islandManager.getIslands()){
+            if (islandTile.equals(islandManager.getIsland(0)) || islandTile.equals(islandManager.getIsland(6))){
+                assertEquals(0, islandTile.count());
+            }else{
+                assertEquals(1, islandTile.count());
             }
         }
 
-        assertEquals(2, size);
-        assertEquals(pawnCurr+pawnNext, pawnCurrAfter);
-
     }
 
+    /**
+     * this method verifies that the moveAllPawnsFrom method works correctly
+     */
     @Test
-    void checkNewOwner(){
-       int green = curr.count(PawnColor.GREEN);
-       int red = curr.count(RED);
-       int blue = curr.count(BLUE);
+    public void moveAllPawnsFrom(){
 
-       if(green> red+blue+size){
-           newOwner = otherPlayer;
-       }else{
-           newOwner = owner;
-       }
+        islandManager.getIsland(2).moveAllPawnsFrom(islandManager.getIsland(3));
 
-        assertEquals(owner, newOwner);
-
-
+        assertEquals(2, islandManager.getIsland(2).count());
+        assertEquals(0, islandManager.getIsland(3).count());
 
     }
 
