6e86c4626b274911edb5c34972a8a437123001dc
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2022-06-09 15:54:34 +0200

    Added method chosenCard to extract and save the number of the assistant card chosen and to display the chosen assistant card in the last played deck

--- src/main/java/it/polimi/ingsw/client/chooseAssistantController.java
+++ src/main/java/it/polimi/ingsw/client/chooseAssistantController.java
@@ -7,9 +7,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class chooseAssistantController {
@@ -36,4 +41,25 @@ public class chooseAssistantController {
         stage.show();
 
     }
+
+    @FXML
+    ImageView card1;
+    @FXML
+    ImageView lastPlayed1;
+
+    private int cardNumber;
+
+    public void chosenCard(){
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(card1.toString());
+        while(m.find()){
+            cardNumber = Integer.parseInt(m.group());
+            System.out.println(""+cardNumber);
+        }
+        Image image = new Image(String.valueOf(getClass().getResource("/Assistente(1)-min.png")));
+        lastPlayed1.setImage(image);
+
+        //TODO: somehow remove assistant card chosen
+    }
+
 }
