519dbe06a20d0438d12ab0e96bded779788aa1c2
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2022-06-11 16:08:27 +0200

    Added constructor method in ChooseAssistantController to link it to ClientGUIFirst. This way ClientGUIFirst can send to server the chosen assistant card, passing through the guiView

--- src/main/java/it/polimi/ingsw/client/ChooseAssistantController.java
+++ src/main/java/it/polimi/ingsw/client/ChooseAssistantController.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.client;
 
+import it.polimi.ingsw.EventManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
@@ -23,8 +24,18 @@ public class ChooseAssistantController {
     private Scene scene;
     private Parent root;
 
-    //TODO: inteleave this with only one instance of ClientGUIFirst
-    private final ClientGUIFirst clientGUIFirst = new ClientGUIFirst();
+    private ClientGUIFirst clientGUIFirst;
+
+    public ChooseAssistantController(ClientGUIFirst clientGUIFirst){
+        this.clientGUIFirst = clientGUIFirst;
+
+
+
+    }
+
+
+
+
 
 
 
@@ -46,7 +57,9 @@ public class ChooseAssistantController {
 
     public void clickedButton(ActionEvent e) throws IOException {
 
-        GameBoardController c = new GameBoardController();
+        gameBoardController c = new gameBoardController();
+
+
 
         root = FXMLLoader.load(getClass().getResource("/GameBoard2.fxml"));
 
@@ -70,61 +83,51 @@ public class ChooseAssistantController {
         number(card1);
         lastPlayed1.setImage(new Image(String.valueOf(getClass().getResource("/Assistente(1)-min.png"))));
         card1.setOpacity(0.4d);
-        clientGUIFirst.updateChosenAssistant();
     }
     public void chosenCard2(){
         number(card2);
         lastPlayed1.setImage(new Image(String.valueOf(getClass().getResource("/Assistente(2)-min.png"))));
         card2.setOpacity(0.4d);
-       clientGUIFirst.updateChosenAssistant();
     }
     public void chosenCard3(){
         number(card3);
         lastPlayed1.setImage(new Image(String.valueOf(getClass().getResource("/Assistente(3)-min.png"))));
         card3.setOpacity(0.4d);
-        clientGUIFirst.updateChosenAssistant();
     }
     public void chosenCard4(){
         number(card4);
         lastPlayed1.setImage(new Image(String.valueOf(getClass().getResource("/Assistente(4)-min.png"))));
         card4.setOpacity(0.4d);
-        clientGUIFirst.updateChosenAssistant();
     }
     public void chosenCard5(){
         number(card5);
         lastPlayed1.setImage(new Image(String.valueOf(getClass().getResource("/Assistente(5)-min.png"))));
         card5.setOpacity(0.4d);
-        clientGUIFirst.updateChosenAssistant();
     }
     public void chosenCard6(){
         number(card6);
         lastPlayed1.setImage(new Image(String.valueOf(getClass().getResource("/Assistente(6)-min.png"))));
         card6.setOpacity(0.4d);
-        clientGUIFirst.updateChosenAssistant();
     }
     public void chosenCard7(){
         number(card7);
         lastPlayed1.setImage(new Image(String.valueOf(getClass().getResource("/Assistente(7)-min.png"))));
         card7.setOpacity(0.4d);
-        clientGUIFirst.updateChosenAssistant();
     }
     public void chosenCard8(){
         number(card8);
         lastPlayed1.setImage(new Image(String.valueOf(getClass().getResource("/Assistente(8)-min.png"))));
         card8.setOpacity(0.4d);
-        clientGUIFirst.updateChosenAssistant();
     }
     public void chosenCard9(){
         number(card9);
         lastPlayed1.setImage(new Image(String.valueOf(getClass().getResource("/Assistente(9)-min.png"))));
         card9.setOpacity(0.4d);
-        clientGUIFirst.updateChosenAssistant();
     }
     public void chosenCard10(){
         number(card10);
         lastPlayed1.setImage(new Image(String.valueOf(getClass().getResource("/Assistente(10)-min.png"))));
         card10.setOpacity(0.4d);
-        clientGUIFirst.updateChosenAssistant();
     }
 
     private int cardNumber;
@@ -135,6 +138,7 @@ public class ChooseAssistantController {
         while (m.find()) {
             cardNumber = Integer.parseInt(m.group());
             System.out.println("" + cardNumber);
+            clientGUIFirst.updateChosenAssistant(cardNumber);
 
         }
 
