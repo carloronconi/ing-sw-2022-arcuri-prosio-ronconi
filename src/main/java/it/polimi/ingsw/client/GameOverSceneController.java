55e19519cfa01fd7a7725996521be7443c5f2a7c
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2022-06-27 18:10:16 +0200

    JavaDoc to GameOverSceneController

--- src/main/java/it/polimi/ingsw/client/GameOverSceneController.java
+++ src/main/java/it/polimi/ingsw/client/GameOverSceneController.java
@@ -6,6 +6,11 @@ import javafx.scene.control.Label;
 public class GameOverSceneController extends SceneController {
     public Label winner;
 
+    /**
+     * Method shows to the players the winner according to the server.
+     * If something went wrong - the connection dropped or a player was disconnected to the game - the scene is shown to the
+     * other players stating "nobody" as winner
+     */
     public void setup(){
         String winnerName = getClientGui().getGuiView().getWinner();
         String message = winnerName == null? "Nobody!" : winnerName;
