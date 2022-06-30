83dbc651c2a53efdf307bb1b67ba939e99f49dd6
carloronconi <carlo.ronconi@mail.polimi.it>
2022-06-30 17:29:40 +0200

    update JARs

--- src/main/java/it/polimi/ingsw/client/gui/LetsPlaySceneController.java
+++ src/main/java/it/polimi/ingsw/client/gui/LetsPlaySceneController.java
@@ -6,7 +6,7 @@ import java.io.IOException;
  * Class to show to the player the starting image of the game
  */
 public class LetsPlaySceneController extends  SceneController{
-    public void starting() throws IOException { //button at the end of eryantisFirstScene - the one with let's play
+    public void starting() throws IOException { //button at the end of eryantis first scene - the one with let's play
         new ChangeScene(getClientGui()).run();
 
     }
